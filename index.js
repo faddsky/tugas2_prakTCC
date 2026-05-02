4ca50086080aacc643866313aa78fa450ac664f3
Fadilah Nur Sabiyyah <154042229+faddsky@users.noreply.github.com>
2026-05-03 01:20:12 +0700

    ganti lagi

--- index.js
+++ index.js
@@ -12,17 +12,7 @@ const app = express();
 
 // 4. Konfigurasi CORS
 // Sesuaikan dengan origin frontend kamu agar tidak kena blokir browser
-app.use(cors({
-  origin: [
-    'http://localhost', 
-    'http://localhost:5173', 
-    'http://127.0.0.1:5500', 
-    'http://localhost:3000',
-    'https://notes-fadilah-dot-b-12-488514.uc.r.appspot.com'
-  ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
+app.use(cors());
 
 // 5. Middleware
 app.use(express.json()); // Untuk membaca body berformat JSON
